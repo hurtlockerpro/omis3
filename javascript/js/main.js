0f24b447a32311d2b4ee83175720856735215261
mihkelva <51737622+mihkelva@users.noreply.github.com>
2020-11-04 20:17:04 +0200

    javascript v1

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -13,6 +13,10 @@ let firstname = 'Vladimir';
 
 let lastname = `result: ${1+ + 2 + 2 + 1}`; // template
 
+const x = 10;
+
+//x = 11;
+
 console.log('result: ' + (1+1 + (5+8) )); // concatenation
 console.log((1+1) + 'result: '); // concatenation
 console.log( 'result: ' + (1+1) + ' end'); // concatenation
@@ -77,9 +81,146 @@ x -= 1
  */
 
 // loop
+
 for (let idx = 0; idx < 5; idx++) 
 {
     console.log(" idx = " + idx);
 }
+
+// 1 element = 0
+// length - 1
+let cars = ['audi', 'bmw', 'citroen', 11111, 4444, true, undefined ];
+cars[3] = 'jaguar';
+
+cars.push('volvo');
+for (let index = cars.length - 1; index >= 0; index--) 
+{
+
+    //if (cars[index] == 'audi')
+    /*
+    if (index <= 2)
+    {
+        console.log('YES this is ' + cars[index]);
+    } else {
+
+        */
+        //console.log('index: ' + index + '; value: ' + cars[index]);
+    //}
+
+   // if (index == 3) break;
+
+}
+//console.log('index: 3 value: ' + cars[3]);
+
+console.log('volvo has index = ' + cars.indexOf('volvo'));
+
+
+cars.forEach((value, index, array) => {
+    console.log(value, index, array[index])
+});
+
+
+let carsObject = {
+    'car': {
+        'type' : 'audi',
+        'color': 'red',
+        'age':'25'
+    },
+    'car2': {
+        'type' : 'bmw',
+        'color': 'black',
+        'age':'5',
+        'wheels': ['1wheel', '2wheel', '4wheels']
+    }
+};
+
+//console.log(cars, ' what is type: ', typeof cars);
+// console.log(carsObject);
+
+// console.log('car2', carsObject.car2);
+// console.log('car2', carsObject.car2.wheels[1]);
+
+let keys = Object.keys(carsObject.car2);
+let values = Object.values(carsObject);
+console.log(
+    //Object.keys(carsObject.car2)[2];
+    //keys[2]
+    //values[1].wheels[2]
+    'entries:',
+    Object.entries(carsObject)
+);
+
+
+Object.entries(carsObject).forEach(
+    function (entry) { // callback
+        const [key, value] = entry;
+        //console.log(key, value, entry.key, entry.value);
+        console.log(entry[1].type);
+    }
+);
+
+
+
+
+/*
+let tmpAge = prompt("Please enter your age?");
+// && - and
+// || - or 
+
+// AND &&
+// true = true && true && true -> (a > 1 && b > 1 && c > 1)
+// false = true && true && false -> (a > 1 && b > 1 && c > 1)
+
+// OR ||
+// true = true || false || false
+// false = false || false || false
+
+let age = parseInt(tmpAge);
+// console.log (typeof age, typeof num );
+
+
+if (age > 18){
+//if (age > 18 && isNaN(age) == false){
+    console.log('age inserted (', age, '): you are adult');
+}
+else if (isNaN(age) == true)
+{
+    console.log('change your age!');
+
+} else {
+    console.log('go away!');
+}
+
+*/
+let x1 = [1,2,3,4,5,6,7,8,9];
+let y1 = [1,2,3,4,5,6,7,8,9];
+
+
+let result = '';
+let cell = `<td>${result}</td>`;
+let row = `<tr>${cell}</tr>`;
+let table = `<table>${row}</table>`;
+
+for (let index_x = 0; index_x <= x1.length - 1; index_x++) 
+{
+    cell = '';
+    for (let index_y = 0; index_y <= y1.length - 1; index_y++) 
+    {
+        //console.log(x1[index_x] + '*' + y1[index_y], '=', x1[index_x] * y1[index_y]);
+        result = x1[index_x] * y1[index_y];
+        cell += `<td>${result}</td>`
+    }
+    row += `<tr>${cell}</tr>`;
+}
+table = `<table>${row}</table>`
+
+
+let div = document.getElementById('calc');
+div.innerHTML = table;
+
+/*
+document.write("Hello World!");
+document.writeln("Hello World!"); // \n
+*/
 
 
