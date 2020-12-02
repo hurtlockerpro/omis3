13c3b95dccbbcb99b633e2b1f69817eb8c9e27ad
mihkelva <51737622+mihkelva@users.noreply.github.com>
2020-12-02 21:04:03 +0200

    nodejs v2

--- node/client/js/books.js
+++ node/client/js/books.js
@@ -1,29 +1,109 @@
 
 const loadBooks = () => {
-    //const xhttp = new XMLHttpRequest();
+
     $.ajax({
         url: 'http://localhost:3000/books',
         success: function(data){
             console.log(data);
 
-            let = books = '';
+            let books = '';
             for (let index = 0; index < data.length; index++) 
             {
                 books += `<div class="card" style="width: 18rem;">
                     <img src="${data[index].bookCover}" class="card-img-top">
                 <div class="card-body">
                     <h5 class="card-title">${data[index].title}</h5>
+                    <p class="card-text">${data[index].isbn}</p>
                     <p class="card-text">${data[index].description}</p>
                     <p class="card-text">${data[index].author}</p>
-                    <a href="#" class="btn btn-warning">edit</a>
-                    <a href="#" class="btn btn-danger">delete</a>
+                    <a href="#" class="btn btn-warning edit" data-isbn="${data[index].isbn}">edit</a>
+                    <a href="#" class="btn btn-danger" onclick="deleteBook(${data[index].isbn})">delete</a>
                 </div>
                 </div>`;
             }
             document.getElementById('bookslist').innerHTML = books;
+        }, 
+        error: function(err, status, message){
+            //
+            if (status == 404){
+                console.log('nmot found');
+            }
         }
     });
 
 };
 
+
+const deleteBook = (isbn) => {
+
+    $.ajax({
+        url: 'http://localhost:3000/book/' + isbn,
+        type: 'DELETE',
+        success: function(data){
+            console.log(data);
+        }
+    });
+}
+
 loadBooks();
+
+// MODAL WINDOW WITH FORM
+
+$(document).on('click', '.edit', function(event){
+
+    let isbn = this.dataset.isbn;
+
+    $('#myModalBook').modal('show');
+
+    $('#myModalBook').on('shown.bs.modal', function () {
+
+        console.log('modal is shown');
+        // get form 
+        $.ajax({
+            url: 'form.html',
+            success: function(data){
+                //console.log(data);
+                let window = document.querySelector('#myModalBook .modal-body').innerHTML = data;
+            }
+        });
+
+        $.ajax({
+            // http://localhost:3000/book/7575757575
+            url: 'http://localhost:3000/book/' + isbn,
+            success: function(data){
+                //console.log(data);
+
+                document.getElementById('isbn').value = data.isbn;
+                document.getElementById('title').value = data.title;
+                document.getElementById('author').value = data.author;
+                document.getElementById('description').value = data.description;
+                document.getElementById('publish_date').value = data.publish_date;
+                document.getElementById('pages').value = data.pages;
+ 
+            }
+        });
+    
+
+    })
+
+});
+
+
+$(document).on('submit', '#btnSubmitForm', function(event){
+
+        event.preventDefault();
+
+        $.ajax({
+            url: 'http://localhost:3000/book/edit/ok', // + isbn,
+            type: 'json',
+            method: 'post',
+            data: $('newBook').serialize(),
+            success: function(data){
+                console.log(data);
+
+            }
+        });
+
+});
+
+
--- node/server/js/server.js
+++ node/server/js/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 let books = [
     {
@@ -38,10 +43,59 @@ let books = [
 app.get('/books', (req, res) => {
     
     console.log('books ...');
-    console.log(books);
+    //console.log(books);
   
     res.send(books); // browser
 });
+
+
+
+app.get('/book/:isbn', (req, res) => {
+    
+    console.log('exact book: ');  
+    console.log(req.params.isbn);
+    
+    books.forEach(element => {
+        if (element.isbn == req.params.isbn)
+        {
+            return res.send(element);
+        }
+    });
   
+    res.status(404).send("Book not found"); // browser
+});
+
+
+app.delete('/book/:isbn', (req, res) => {
+    
+    console.log('exact book: ');  
+    console.log(req.params.isbn);
+    
+    books.forEach((element, index) => {
+        if (element.isbn == req.params.isbn)
+        {
+            books.splice(index, 1);
+            //return res.send(element);
+        }
+    });
+  
+    res.status(200).send("Book deleted successfully"); // browser
+});
+
+app.post('/book/edit/ok', (req, res) => {
+    
+    console.log('new data: ');  
+    console.log(req.body);
+    /*
+    books.forEach(element => {
+        if (element.isbn == req.params.isbn)
+        {
+            return res.send(element);
+        }
+    });
+  
+    res.status(404).send("Book not found"); // browser
+    */
+});
 
 app.listen(port, () => console.log('Books app is working OK!'));
